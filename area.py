52ba34abadee05d04b4537bec0333b401234c432
Francesco Garcia <fgarciareyes@students.mchenry.edu>
2025-08-28 11:37:16 -0500

    From class

---
+++ area.py
@@ -0,0 +1,16 @@
+"""
+Write a Python program to calculate the area of a rectangle:
+Declare two variables, length and width, and assign values to them.
+Calculate the area using the formula: area = length * width.
+Print a message displaying the dimensions and the area using concatenation or formatting.
+"""
+
+# Declare height and width and assign numbers to them
+length = 5
+width = 3
+
+# Calculate the area = length * width
+area = length * width
+
+# Print a message displaying the dimensions and the area
+print("A rectangle with length of", length, "and width of", width, "has an area of", area)
