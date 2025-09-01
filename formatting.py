08425271666b125f10f31950021f8b07c0e96ff6
Francesco <fgarciareyes@students.mchenry.edu>
2025-09-01 16:58:25 -0500

    Add files via upload

---
+++ formatting.py
@@ -0,0 +1,20 @@
+# Define variables for kilogram values
+kg_value_1 = 1
+kg_value_2 = 2
+kg_value_3 = 3
+kg_value_4 = 4
+
+# Conversion factor: 1 kilogram = 2.20462 pounds
+conversion_factor = 2.20462
+
+# Calculate pounds for each kilogram value
+lb_value_1 = kg_value_1 * conversion_factor
+lb_value_2 = kg_value_2 * conversion_factor
+lb_value_3 = kg_value_3 * conversion_factor
+lb_value_4 = kg_value_4 * conversion_factor
+
+# Output the results
+print(f"{kg_value_1} kilogram is equal to {lb_value_1} pound")
+print(f"{kg_value_2} kilogram is equal to {lb_value_2} pound")
+print(f"{kg_value_3} kilogram is equal to {lb_value_3} pound")
+print(f"{kg_value_4} kilogram is equal to {lb_value_4} pound")
