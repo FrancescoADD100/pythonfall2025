6b308a1f63e444013ca25aea337f64322c6d0017
Francesco Garcia <fgarciareyes@students.mchenry.edu>
2025-09-16 11:30:07 -0500

    fromclass

---
+++ Statements.py
@@ -0,0 +1,13 @@
+"""
+Write the program "99 Bottles of Beer on the Wall" using a while loop. 
+Be mindful to change the word 'bottles' to 'bottle' when down to the last one. 
+You must do the full 99 bottles; the sample shows the last 3 verses.
+"""
+# Type out while loop for 99 bottle of beer on the wall
+count = 99 
+while count <=1: print(count, "Bottles of beer on the wall")
+print(count, "Bottles of beer")
+print("Take one down, pass it around")
+#Decrease amount
+count -1 
+print(count, "Bottles of beer on the wall!")
