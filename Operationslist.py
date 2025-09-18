42092707798ed630193d44d1b93b496bf5906fda
Francesco Garcia <fgarciareyes@students.mchenry.edu>
2025-09-18 11:27:42 -0500

    fromClass

---
+++ Operationslist.py
@@ -0,0 +1,15 @@
+"""
+Objective: Develop a program to manage ticket sales for an event.
+Assignment Steps: Create a list representing 20 seats, numbered 1 to 20.
+Display the list of available seats to the customer.
+Prompt the customer to select a seat by entering its number. Entering '0' ends the purchase process.
+Remove the selected seat from the list of available seats and display the updated list.
+If the customer selects an invalid or already sold seat, display a friendly error message and prompt them to try again.
+Ensure the program gracefully handles all scenarios and is user-friendly.
+"""
+
+# List number of seats
+seat_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+  
+
