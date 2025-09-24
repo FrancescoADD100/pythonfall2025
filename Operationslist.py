e0b30d9f07897c36e2d20aaa7ad73b6e4efeda9f
Francesco <fgarciareyes@students.mchenry.edu>
2025-09-23 23:42:01 -0500

    Add files via upload

--- Operationslist.py
+++ Operationslist.py
@@ -9,7 +9,24 @@ Ensure the program gracefully handles all scenarios and is user-friendly.
 """
 
 # List number of seats
-seat_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for seat in seats:
+    print("available seats are 1-20")
+    if seat:
+        print(f"this seat is available {seat}")
+    else:
+        print("This seat is not available")
+        number = 0
+        while number >= 1 or number <= 20:
+            number = int(input(f"You purchased number {seat}"))
+            if number < 1 or number > 20:
+                print(f"{number} is not available")
+                seats.remove(number)
+                for seat in seats:
+                    print(seat)
 
-  
+
+
+
+
 
