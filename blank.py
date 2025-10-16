06fad56065cce1f627065e0f139adf54538d0928
Francesco Garcia <fgarciareyes@students.mchenry.edu>
2025-10-16 11:26:04 -0500

    from class

--- blank.py
+++
@@ -1,5 +0,0 @@
-# TODO: Use the center() method to center the string in a string of specified width
-# Example: "python" -> " python "
-string2 = "python"
-# Your code here
-print(string2.center(8," "))
