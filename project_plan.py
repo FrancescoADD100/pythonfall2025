5d43f0933352f6995b81a3a80fde19b4864082c5
Francesco <fgarciareyes@students.mchenry.edu>
2025-11-11 22:08:14 -0600

    Add files via upload

---
+++ project_plan.py
@@ -0,0 +1,94 @@
+"""
+Project Plan: Chipotle Ordering System
+Student Name: Francesco Garcia
+Course: ADD-100
+Section: 004
+Date: November 2025
+
+Project Description:
+Chipotle ordering system. 
+Customer can view a menu, select an item, choose ingredients, calculate 
+the total price into a receipt.txt file.
+"""
+
+# Class stubs
+
+class MenuItem:
+    # Represents a menu item at Chipotle.
+    # Attributes
+    # Methods
+    def __init__(self, name, price):
+        # Initialize the name and price
+       
+        
+
+    def display_info(self):
+        # Prints the menu item and its price
+        
+
+class Order:
+    # Represents a customer order.
+    # Attributes
+    # Methods
+    def __init__(self, main_item, rice, beans, protein):
+        
+
+    def add_extra(self, extra):
+        # Adds an extra item to the order
+        
+
+    def calculate_total(self):
+        # Calculates the total price of the order
+        
+
+    def show_order(self):
+        # Displays the order details
+        
+
+class ChipotleSystem:
+    # Represents the ordering system.
+    # Attributes
+    # Methods
+    def __init__(self):
+        
+
+    def show_menu(self):
+        # Displays all items in the menu
+        
+
+    def take_order(self, main_item, rice, beans, protein):
+        # Creates a new order with the selected items
+        
+
+    def display_total(self):
+        # Displays the total price of the current order
+        
+
+# Function stubs
+
+def directions():
+    # Print a welcome message and directions to the user
+        
+
+def get_main_item():
+    # Ask the user to select the main item (Bowl/Burrito)
+        
+
+def get_rice():
+    # Ask the user to select rice type (White/Brown)
+        
+
+def get_beans():
+    # Ask the user to select beans (Black/Pinto)
+        
+
+def get_protein():
+    # Ask the user to select protein (Chicken/Beef/Vegetarian)
+        
+
+def get_extras():
+    # Ask the user to select extra items (Guacamole, Extra Protein)
+        
+
+def main():
+    
